8008f4c4b46b48de2cb28c8024d95900910e68bd
deathbegin <jiyinshanxiaobojue@gmail.com>
2021-10-14 23:48:21 +0800

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# NOj-python
+NWPU
+高级python程序设计oj题目
---
+++ season1.py
@@ -0,0 +1,31 @@
+
+a = int(input())
+b = int(input())
+c = input()
+if ('+' == c):
+    d = a+b
+elif ('-' == c):
+    d = a-b
+elif ('*' == c):
+    d = a*b
+elif ('/' == c):
+    d = a/b
+elif ('//' == c):
+    d = a//b
+elif ('%' == c):
+    d = a % b
+elif ('**' == c):
+    d = a**b
+else:
+    d = False
+# ops = {'+': operator.add,
+#        '-': operator.sub,
+#        '*': operator.mul,
+#        '/': operator.truediv,
+#        '//': operator.floordiv,
+#        '%': operator.mod,
+#        '**': operator.pow}
+if (d == False):
+    print("ERROR")
+else:
+    print('{}{}{}{}{}'.format(a, c, b, "=", d))
