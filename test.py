bcfcfb4c6b38132f0ebdd95be695de6442bfbbc1
deathbegin-mechrev <jiyinshanxiaobojue@gmail.com>
2021-12-05 22:34:41 +0800

    仅剩T78和T84

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
-# T075, s8-5
+# T080, s8-10
 import pandas as pd
+import numpy as np
 
-n = int(input())
 table = []
-for i in range(5*n):
+for i in range(9):
     t = list(input().split())
     table.append(t)
-df = pd.DataFrame(table, columns=["Mon", "Part", "Num", "Price"])
-print(df)
+df = pd.DataFrame(table, columns=["Mon", "Group", "Sales"])
+df[["Mon", "Sales"]] = df[["Mon", "Sales"]].astype(np.int64)
 
-df_new = pd.pivot_table(df, index=["Mon", "Part"]).T
-print(df_new)
+df_group = df.groupby(["Group"])["Sales"].sum()
+print(df_group)
+df_Mon = df.groupby(["Mon"])["Sales"].sum()
+print(df_Mon)
